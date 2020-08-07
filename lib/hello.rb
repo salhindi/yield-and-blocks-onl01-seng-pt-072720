1d9a5f3134d5bd0afa2a407e72cdfd63d2db8563
Sara A <calisaraalhindi@gmail.com>
2020-08-07 20:12:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,6 +7,10 @@ end
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 # i = counter vriable
 # incrememnted the counter in the while loop, only executed if i > the length of the array

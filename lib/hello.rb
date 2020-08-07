5db8374a3920a29a8b929bd87cc720339da7ea3b
Sara A <calisaraalhindi@gmail.com>
2020-08-07 20:12:21 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,12 @@
 def hello_t(array)
   i= 0
   while i < array.length
-  i= i + i
+  yield array[i]
+  i += 1
 end
 end
 
 # call your method here!
 
+# i = counter vriable
+# incrememnted the counter in the while loop, only executed if i > the length of the array
